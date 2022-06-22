d53708813bf36fb4ab732c9c21e7eeaf655b81de
augusto-salto <69981764+augusto-salto@users.noreply.github.com>
2022-06-22 12:52:45 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,13 @@ uint8_t countError = 0;
 ELM327 myELM327;
 //teste2
 
-uint32_t rpm = 0;
 
+float rpm = 0;
+float temperatura = 0;
+float posAcel = 0;
+
+float requestElmFloat(float function);
+void errorElm();
 
 void setup()
 {
@@ -32,7 +37,7 @@ void setup()
   }
   
 
-  if (!myELM327.begin(ELM_PORT, true, 5000))
+  if (!myELM327.begin(ELM_PORT, true, 3000))
   {
     Serial.println("Couldn't connect to OBD scanner - Phase 2");
     while (1);
@@ -47,23 +52,35 @@ void setup()
 
 void loop()
 {
-  float tempRPM = myELM327.rpm();
-  delay(200);
-  float engineTemp = myELM327.engineCoolantTemp();
-  delay(200);
-  float posAcel = myELM327.throttle();
-  delay(200);
+  rpm = requestElmFloat(myELM327.rpm());
+  temperatura = requestElmFloat(myELM327.engineCoolantTemp());
+  posAcel = requestElmFloat(myELM327.throttle());
 
-  if (myELM327.nb_rx_state == ELM_SUCCESS)
+  Serial.print("\nRPM: " + (String)rpm);
+  Serial.print("\nTEMPERATURA: " + (String)temperatura);
+  Serial.print("\nACELERADOR: " + (String)posAcel);
+  delay(100);
+    
+}
+
+float requestElmFloat(float function){
+  float temp = function;
+
+if (myELM327.nb_rx_state == ELM_SUCCESS)
   {
-    Serial.print("\nThrottle: "); Serial.print(posAcel);
-    Serial.print("\nEngine Temp: "); Serial.print(engineTemp);
-    rpm = (uint32_t)tempRPM;
-    Serial.print("\nRPM: "); Serial.println(rpm);
+    return temp;
   }
   else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
   {
-    myELM327.printError();
+    errorElm(); 
+    return 99.9;
+  }
+
+}
+
+void errorElm(){
+
+       myELM327.printError();
     Serial.print("\n STATUS BLUETOOTH: ");
     Serial.print(ELM_PORT.connected());
     Serial.print("\n STATUS CONEXAO ELM327: ");
@@ -78,12 +95,8 @@ void loop()
     }else{
       Serial.print("\nTentando reconexao com a ECU");
       countError = 0;
-      myELM327.begin(ELM_PORT, false, 5000);
+      myELM327.begin(ELM_PORT, false, 3000);
       
     }
-    
-    
-    
-  }
-    
+
 }
