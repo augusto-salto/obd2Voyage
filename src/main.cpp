7697adbb9f2290fda65daaa9d73b3a53479b83ab
augusto-salto <69981764+augusto-salto@users.noreply.github.com>
2022-06-22 12:07:09 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ BluetoothSerial SerialBT;
 #define ELM_PORT   SerialBT
 #define DEBUG_PORT Serial
 
+uint8_t countError = 0;
 
 ELM327 myELM327;
 //teste2
@@ -24,34 +25,65 @@ void setup()
   //SerialBT.setPin("1234");
   ELM_PORT.begin("ArduHUD", true);
   
-  if (!ELM_PORT.connect("OBDII"))
-  {
-    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
-    while(1);
+  while(!ELM_PORT.connected()){
+    ELM_PORT.connect("OBDII");
+    Serial.print(".");
+    delay(200);
   }
+  
 
-  if (!myELM327.begin(ELM_PORT, true, 2000))
+  if (!myELM327.begin(ELM_PORT, true, 5000))
   {
     Serial.println("Couldn't connect to OBD scanner - Phase 2");
     while (1);
   }
 
   Serial.println("Connected to ELM327");
+  delay(1000);
+  myELM327.debugMode = false;
+  
 }
 
 
 void loop()
 {
   float tempRPM = myELM327.rpm();
-  
+  delay(200);
+  float engineTemp = myELM327.engineCoolantTemp();
+  delay(200);
+  float posAcel = myELM327.throttle();
+  delay(200);
 
   if (myELM327.nb_rx_state == ELM_SUCCESS)
   {
+    Serial.print("\nThrottle: "); Serial.print(posAcel);
+    Serial.print("\nEngine Temp: "); Serial.print(engineTemp);
     rpm = (uint32_t)tempRPM;
-    Serial.print("RPM: "); Serial.println(rpm);
+    Serial.print("\nRPM: "); Serial.println(rpm);
   }
   else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+  {
     myELM327.printError();
+    Serial.print("\n STATUS BLUETOOTH: ");
+    Serial.print(ELM_PORT.connected());
+    Serial.print("\n STATUS CONEXAO ELM327: ");
+    Serial.print(myELM327.connected);
+    Serial.print("\n");
+    
+    if(countError <= 5){
+      countError++;
+    Serial.print("\nCount Errors: ");
+    Serial.print(countError);
 
-    delay(100);
+    }else{
+      Serial.print("\nTentando reconexao com a ECU");
+      countError = 0;
+      myELM327.begin(ELM_PORT, false, 5000);
+      
+    }
+    
+    
+    
+  }
+    
 }
